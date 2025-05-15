11f8d3a92c5370f78f18e00f92602d80a0f39f7b
Yohanan-Negash <yonidigital896@gmail.com>
2025-05-14 19:05:45 -0500

    better logging

--- app/jobs/delete_expired_quick_notes_job.rb
+++ app/jobs/delete_expired_quick_notes_job.rb
@@ -2,6 +2,8 @@ class DeleteExpiredQuickNotesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    QuickNote.where("expires_at < ?", Time.current).destroy_all
+    expired_notes = QuickNote.where("expires_at < ?", Time.current).destroy_all
+    deleted_notes = expired_notes.size
+    Rails.logger.info("[DeleteExpiredQuickNotesJob] Deleted #{deleted_notes} expired quick notes at #{Time.current}.")
   end
 end
